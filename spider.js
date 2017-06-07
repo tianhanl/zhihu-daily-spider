ac43aed2ba06d5bcee312528abb6e80839dc1e78
Tianhang Liu <tianhanl@usc.edu>
2017-06-07 14:58:56 +0800

    split request the latest stories from daily.zhiu into a separate function

--- spider.js
+++ spider.js
@@ -3,62 +3,79 @@ var cheerio = require('cheerio');
 var mongo = require('./mongo');
 
 
-var Story = function(link, title, imageSrc, time) {
+const Story = function (link, title, imageSrc, time) {
     this.link = link;
     this.title = title;
     this.imageSrc = imageSrc;
     this.time = time;
 };
 
-module.exports = {
-    requestStoryList: function() {
+const getLatestStories = function () {
+    return new Promise(function (resolve, reject) {
         var date = new Date();
-        var currentTime = '' + (date.getMonth() + 1) + '/' + date.getDate;
-        mongo.ZhihuStory.find({ time: currentTime }, function(err, stories) {
+        var currentTime = '' + date.getFullYear() +
+            '/' + (date.getMonth() + 1) +
+            '/' + date.getDate();
+        superagent.get('https://daily.zhihu.com')
+            .end(function (err, pres) {
+                if (err) {
+                    reject(err);
+                }
+                var stories = [];
+                var $ = cheerio.load(pres.text);
+                var $boxes = $('.main-content-wrap .box');
+                $boxes.each(function (index, element) {
+                    var $self = $(this);
+                    var $linkButton = $self.children('.link-button');
+                    var link = $linkButton.attr('href');
+                    var title = $linkButton.children('.title').text();
+                    var imageSrc = $linkButton.children('.preview-image').attr('src');
+                    var currentStory = new Story(link, title, imageSrc, currentTime);
+                    stories.push(currentStory);
+                });
+                resolve(stories);
+            });
+    });
+};
+
+
+
+module.exports.requestStoryList = function () {
+    var date = new Date();
+    var result;
+    var currentTime = '' + date.getFullYear() +
+        '/' + (date.getMonth() + 1) +
+        '/' + date.getDate();
+    return new Promise(function (resolve, reject) {
+        mongo.ZhihuStory.find({ time: currentTime }, function (err, stories) {
             if (err) {
                 console.log(err);
             }
             if (stories.length > 0) {
-                return Promise.resolve(stories.map(function(story) {
-                    return {
-                        link: story.link,
-                        title: story.title,
-                        imageSrc: story.imageSrc,
-                        time: story.time
-                    };
-                }));
+                resolve(
+                    stories.map(function (story) {
+                        return {
+                            link: story.link,
+                            title: story.title,
+                            imageSrc: story.imageSrc,
+                            time: story.time
+                        };
+                    }));
             } else {
-                return new Promise(function(resolve, reject) {
-                    superagent.get('https://daily.zhihu.com')
-                        .end(function(err, pres) {
-                            if (err) {
-                                reject(err);
-                            }
-                            var stories = [];
-                            var $ = cheerio.load(pres.text);
-                            var $boxes = $('.main-content-wrap .box');
-                            $boxes.each(function(index, element) {
-                                var $self = $(this);
-                                var $linkButton = $self.children('.link-button');
-                                var link = $linkButton.attr('href');
-                                var title = $linkButton.children('.title').text();
-                                var imageSrc = $linkButton.children('.preview-image').attr('src');
-                                var currentStory = new Story(link, title, imageSrc, currentTime);
-                                var mongoStory = new mongo.ZhihuStory({
-                                    link: currentStory.link,
-                                    title: currentStory.title,
-                                    imageSrc: currentStory.imageSrc,
-                                    time: currentStory.time
-                                });
-                                mongoStory.save(function(err) {
-                                    console.log(err);
-                                });
-                                stories.push(currentStory);
-                            });
-                            resolve(stories);
+                getLatestStories().then(function (stories) {
+                    stories.forEach(function (story) {
+                        var mongoStory = new mongo.ZhihuStory({
+                            link: story.link,
+                            title: story.title,
+                            imageSrc: story.imageSrc,
+                            time: story.time
                         });
+                        mongoStory.save(function (err) {
+                        });
+                    });
+                    resolve(stories);
                 });
             }
         });
-    }
+    }); // end of return Promise
 };

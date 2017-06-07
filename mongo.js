2bc8d215fb4ffd72bdebffdbc698b2ef1dbbeee9
Tianhang Liu <tianhanl@usc.edu>
2017-06-07 14:24:24 +0800

    change the promise plugin for mongoose to global.promise

--- mongo.js
+++ mongo.js
@@ -1,13 +1,17 @@
 let mongoose = require('mongoose');
-let ZhihuStory = require('./models/ZhihuStorys');
+let ZhihuStory = require('./models/ZhihuStory');
 
 let db = mongoose.connect('mongodb://localhost/zhihu');
 
-db.connection.on('error', function(err) {
+// Since the default mongoose promise library is deprecated, change to use the
+// global promise object
+mongoose.Promise = global.Promise;
+
+db.connection.on('error', function (err) {
     console.log('Cannot connect to database ${err}');
 });
 
-db.connection.on('open', function() {
+db.connection.on('open', function () {
     console.log('Connected');
 });
 
